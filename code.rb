3fbf99e337cd02566b209ab5277082f3f59d6025
EthanHellman <emanhellman@gmial.com>
2015-06-25 14:30:34 +0000

    I just commited the code.rb file

---
+++ code.rb
@@ -0,0 +1,101 @@
+def encryption(string)
+ 
+  def letter(string)
+    string.to_i = @string
+    
+    def letters 
+      
+    end
+    
+    def letters2
+      
+    end
+  
+    def letters3
+    
+    end
+  
+    def letters4
+    
+    end
+  
+    def letters5
+    
+    end
+  
+    def letters6
+    
+    end
+  
+    def letters7
+    
+    end
+    
+    if 
+      
+      
+    end
+    
+  end
+  
+  
+  
+  def number(string)
+    string.to_i = @string
+    
+    def number 
+      
+    end
+    
+    def numbers2
+    
+    end
+  
+    def numbers3
+    
+    end
+  
+    def numbers4
+    
+    end
+    
+  end
+  
+  def symbol(string)
+    string.to_i = @string
+    
+    def symbols
+      
+    end
+    
+    def symbols2
+    
+    end
+  
+    def symbols3
+    
+    end
+  
+    def symbols4
+    
+    end
+    
+  end
+  
+
+  
+#   if string.length < 5 && string.length > 0
+#     letters
+#   elsif string.length > 6 && string.length < 10
+    
+#   elsif string.length > 11 && string.length < 16
+#     symbols
+#   elsif string.length > 17 && string.length < 21
+#     letters2
+#   elsif string.legnth > 
+  
+  
+  
+  
+  
+end 
